0a44ecb4c6af09dca597c21810ef9589fb6e677c
Icaro Pereira <icarooliv2@gmail.com>
2021-08-09 20:47:30 -0300

    adds draw, show and traverse algorithms
    Co-authored-by: Augusto Vilarins <augusto.vilarins@gmail.com>

--- maze.js
+++ maze.js
@@ -25,6 +25,41 @@ class Maze {
     current = this.grid[0][0];
     this.grid[this.rows - 1][this.columns - 1].goal = true;
   }
+
+  draw() {
+    maze.width = this.size;
+    maze.height = this.size;
+    maze.style.background = "black";
+    current.visited = true;
+    for (let row = 0; row < this.rows; row++) {
+      for (let column = 0; column < this.columns; column++) {
+        let grid = this.grid;
+        grid[row][column].show(this.size, this.rows, this.columns);
+      }
+    }
+    let next = current.checkNeighbours();
+    
+    if (next) {
+      next.visited = true;
+      this.stack.push(current);
+      current.highlight(this.columns);
+      current.removeWalls(current, next);
+      current = next;
+    } else if (this.stack.length > 0) {
+      let cell = this.stack.pop();
+      current = cell;
+      current.highlight(this.columns);
+    }
+
+    if (this.stack.length === 0) {
+      generationComplete = true;
+      return;
+    }
+
+    window.requestAnimationFrame(() => {
+      this.draw();
+    });
+  }
 }
 
 class Cell {
@@ -42,6 +77,31 @@ class Cell {
     this.parentGrid = parentGrid;
     this.parentSize = parentSize;
   }
+
+  checkNeighbours() {
+    let grid = this.parentGrid;
+    let row = this.rowNum;
+    let col = this.colNum;
+    let neighbours = [];
+
+    let top = row !== 0 ? grid[row - 1][col] : undefined;
+    let right = col !== grid.length - 1 ? grid[row][col + 1] : undefined;
+    let bottom = row !== grid.length - 1 ? grid[row + 1][col] : undefined;
+    let left = col !== 0 ? grid[row][col - 1] : undefined;
+
+    if (top && !top.visited) neighbours.push(top);
+    if (right && !right.visited) neighbours.push(right);
+    if (bottom && !bottom.visited) neighbours.push(bottom);
+    if (left && !left.visited) neighbours.push(left);
+
+    if (neighbours.length !== 0) {
+      let random = Math.floor(Math.random() * neighbours.length);
+      return neighbours[random];
+    } else {
+      return undefined;
+    }
+  }
+
   drawTopWall(x, y, size, columns, rows) {
     ctx.beginPath();
     ctx.moveTo(x, y);
@@ -69,6 +129,19 @@ class Cell {
     ctx.lineTo(x, y + size / rows);
     ctx.stroke();
   }
+
+  highlight(columns) {
+    let x = (this.colNum * this.parentSize) / columns + 1;
+    let y = (this.rowNum * this.parentSize) / columns + 1;
+    ctx.fillStyle = "white";
+    ctx.fillRect(
+      x,
+      y,
+      this.parentSize / columns - 3,
+      this.parentSize / columns - 3
+    );
+  }
+
   removeWalls(cell1, cell2) {
     let x = cell1.colNum - cell2.colNum;
     if (x === 1) {
@@ -89,7 +162,22 @@ class Cell {
     }
   }
 
+  show(size, rows, columns) {
+    let x = (this.colNum * size) / columns;
+    let y = (this.rowNum * size) / rows;
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "black";
+    ctx.lineWidth = 2;
+    if (this.walls.topWall) this.drawTopWall(x, y, size, columns, rows);
+    if (this.walls.rightWall) this.drawRightWall(x, y, size, columns, rows);
+    if (this.walls.bottomWall) this.drawBottomWall(x, y, size, columns, rows);
+    if (this.walls.leftWall) this.drawLeftWall(x, y, size, columns, rows);
+    if (this.visited) {
+      ctx.fillRect(x + 1, y + 1, size / columns - 2, size / rows - 2);
+    }
+    if (this.goal) {
+      ctx.fillStyle = "rgb(83, 247, 43)";
+      ctx.fillRect(x + 1, y + 1, size / columns - 2, size / rows - 2);
+    }
+  }
 }
-
-let newMaze = new Maze(300, 20, 10);
-newMaze.setup();
